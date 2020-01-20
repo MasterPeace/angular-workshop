2f44b5f5362d7882e96f04e9c2a50ccb5de58b3f
Eric Thomas <e01010100@users.noreply.github.com>
2020-01-19 19:49:55 -0500

    Dependency updates (#3)
    * updated angular core version
    
    * updated dependencies to avoid vulnerable libraries
    
    * updated format and linting rules

--- workshop/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ workshop/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('./pages/landing-page/landing-page.module').then(mod => mod.LandingPageModule)}
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/landing-page/landing-page.module').then(
+        mod => mod.LandingPageModule
+      )
+  }
 ];
 
 @NgModule({
--- workshop/angular-tour-of-heroes/src/app/pages/landing-page/landing-page.module.ts
+++ workshop/angular-tour-of-heroes/src/app/pages/landing-page/landing-page.module.ts
@@ -5,13 +5,8 @@ import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
 import { LoadingSpinnerModule } from 'common/loading-spinner';
 
-
 @NgModule({
   declarations: [LandingPageComponent],
-  imports: [
-    CommonModule,
-    LandingPageRoutingModule,
-    LoadingSpinnerModule
-  ]
+  imports: [CommonModule, LandingPageRoutingModule, LoadingSpinnerModule]
 })
-export class LandingPageModule { }
+export class LandingPageModule {}
