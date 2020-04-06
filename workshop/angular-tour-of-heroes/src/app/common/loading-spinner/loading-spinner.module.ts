13f7e86d3bfeec1d756eec3a441a9d21b0607eb2
Eric Thomas <e01010100@users.noreply.github.com>
2020-04-05 20:38:35 -0400

    Angular 9 (#5)
    * update 1
    
    * update 2
    
    * update 2

--- workshop/angular-tour-of-heroes/src/app/common/loading-spinner/loading-spinner.module.ts
+++ workshop/angular-tour-of-heroes/src/app/common/loading-spinner/loading-spinner.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingSpinnerComponent } from './loading-spinner.component';
-import { MatProgressBarModule } from '@angular/material';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   imports: [CommonModule, MatProgressBarModule],
